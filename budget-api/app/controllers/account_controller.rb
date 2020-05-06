71903ca108cdc5cabcf0977dc570f5e4394109a3
thedannysoto <87dsoto@gmail.com>
2020-05-06 15:39:53 -0500

    Adds index, show, create, update, destroy actions to AccountController and CategoryController

--- budget-api/app/controllers/account_controller.rb
+++ budget-api/app/controllers/account_controller.rb
@@ -1,2 +1,36 @@
 class AccountController < ApplicationController
+    def index 
+        accounts = Account.all 
+
+        render json: accounts, status: 200
+    end
+
+    def show
+        account = Account.find(params[:id])
+
+        render json: account, status: 200
+    end
+
+    def create
+        account = Account.create(account_params)
+
+        render json: account, status :200
+    end
+
+    def update
+        account = Account.find(params[:id])
+        account.update(account_params)
+        render json: account, status: 200
+    end
+
+    def destroy
+        account = Account.find(params[:id])
+        account.delete
+    end
+
+    private 
+
+    def account_params 
+        params.require(:account).permit(:name, :balance)
+    endaccounts
 end
--- budget-api/app/controllers/category_controller.rb
+++ budget-api/app/controllers/category_controller.rb
@@ -1,2 +1,36 @@
 class CategoryController < ApplicationController
+    def index 
+        categories = Category.all 
+
+        render json: categories, status: 200
+    end
+
+    def show
+        category = Category.find(params[:id])
+
+        render json: category, status: 200
+    end
+
+    def create
+        category = Category.create(category_params)
+
+        render json: category, status :200
+    end
+
+    def update
+        category = Category.find(params[:id])
+        category.update(category_params)
+        render json: category, status: 200
+    end
+
+    def destroy
+        category = Category.find(params[:id])
+        category.delete
+    end
+
+    private 
+
+    def category_params 
+        params.require(:category).permit(:name, :budgeted, :activity, :available)
+    end
 end
