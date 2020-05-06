e4e68b963180d1f6f8ac2923b6dcb330ee317a50
thedannysoto <87dsoto@gmail.com>
2020-05-06 14:21:55 -0500

    Removes BudgetController

--- budget-api/app/controllers/transaction_controller.rb
+++ budget-api/app/controllers/transaction_controller.rb
@@ -1,2 +1,23 @@
 class TransactionController < ApplicationController
+    def index 
+        transactions = Transaction.all 
+
+        render json: transactions, status: 200
+    end
+
+    def show
+        transaction = Transaction.find(params[:id])
+
+        render json: transaction, status: 200
+    end
+
+    def create
+        transaction = Transaction.create(transaction_params)
+    end
+
+    private 
+
+    def transaction_params 
+        params.require(:transaction).permit()
+    end
 end
