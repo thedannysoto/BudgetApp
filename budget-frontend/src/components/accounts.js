a311a3597b4bc205490555530059215e66ca92ee
thedannysoto <87dsoto@gmail.com>
2020-05-12 14:01:05 -0500

    Updates main page styles and category table styles in budget-frontend

--- budget-frontend/src/components/accounts.js
+++ budget-frontend/src/components/accounts.js
@@ -8,7 +8,17 @@ class Accounts {
 
     fetchAndLoadAccounts() {
         this.adapter.getAccounts().then(accounts => {
-            console.log(accounts)
+            console.log(accounts[0])
+            const list = document.getElementById("accounts-list");
+            for(let account of accounts) {
+                const div = document.createElement("div");
+                div.classList.add("w-full", "hover:bg-indigo-500", "py-3", "text-white");
+                const p = document.createElement("p");
+                p.innerText = account.name;
+                p.classList.add("account-name", "w-full", "font-bold", "pl-4");
+                div.appendChild(p);
+                list.appendChild(div);
+            }
             
             
 
--- budget-frontend/src/components/categories.js
+++ budget-frontend/src/components/categories.js
@@ -10,14 +10,25 @@ class Categories {
         this.adapter.getCategories().then(categories => {
             console.log(categories)
             
+            const main = document.getElementById("main");
+            const div = document.createElement("div");
+            const title = document.createElement("h2");
+            title.classList.add("text-indigo-800", "font-bold", "heading");
+            title.innerText = "Budget Categories"; 
+            div.classList.add("w-full", "h-16", "pt-4", "pl-4", "bg-indigo-200");
+            div.appendChild(title);
+            main.appendChild(div);
+
+
+
             const tableData = [];
             let category;
             for(category of categories) {
                 tableData.push(category);
             }
             
-            const table = new Tabulator("#example-table", {
-                height:205,
+            const table = new Tabulator("#categories-table", {
+                height:832,
                 data:tableData,
                 layout:"fitColumns",
                 columns:[
@@ -27,6 +38,9 @@ class Categories {
                     {title:"Available", field:"available", formatter:"money", hozAlign:"center"}
                 ]
             });
+
+            const catTable = document.getElementById("categories-table");
+            main.appendChild(catTable);
 
         });
     }
